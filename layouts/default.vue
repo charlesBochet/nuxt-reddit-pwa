033cc125cedde22abee5eff3907a2091b2ed9bcc
Miniplop <kevinj@theodo.fr>
2018-12-08 19:19:13 +0100

    Add nav header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <Header />
+    <div class="nav-container" style="max-width:100%">
+      <div>
+        <nuxt-link class="nav-container__title" to="/">Posts</nuxt-link>
+      </div>
+    </div>
     <nuxt/>
   </div>
 </template>
@@ -16,6 +21,11 @@
 </script>
 
 <style>
+
+a {
+  text-decoration: none;
+}
+
 html {
   font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
@@ -25,6 +35,31 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+}
+
+.nav-container {
+  -webkit-box-align: center;
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  min-width: 260px;
+  margin: 0px auto;
+}
+
+.nav-container__title {
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 32px;
+  display: inline-block;
+  padding-bottom: 1px;
+  padding-top: 4px;
+  padding-left: 8px;
+  padding-right: 8px;
+  color: rgb(0, 121, 211);
+  border-bottom: 3px solid rgb(0, 121, 211);
+  margin: 0px 5px;
 }
 
 *, *:before, *:after {
