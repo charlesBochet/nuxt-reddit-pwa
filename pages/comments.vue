d907dfe26545a2533b363d6f84d8fab0e359e3c4
Charles BOCHET <accounts@charlesbochet.me>
2018-12-08 19:34:36 +0100

    Add API endpoints for reddit

--- pages/comments.vue
+++ pages/comments.vue
@@ -4,9 +4,13 @@
 </template>
 
 <script>
+import { getComments } from '~/services/redditClient.js'
 
 export default {
   components: {
+  },
+  mounted() {
+    getComments('pwa', 'a408bd/you_need_neither_pwa_nor_amp_to_make_your_website')
   }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,13 @@
 </template>
 
 <script>
+import { getNewListings } from '~/services/redditClient.js'
 
 export default {
   components: {
+  },
+  mounted() {
+    getNewListings('pwa')
   }
 }
 </script>
