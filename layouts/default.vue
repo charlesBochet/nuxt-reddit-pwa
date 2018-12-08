c00085fe9a4df2bf6bc25ef9bc4b90c0ec94a4ad
Miniplop <kevinj@theodo.fr>
2018-12-08 19:12:16 +0100

    Add header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <Header />
     <nuxt/>
   </div>
 </template>
+
+<script>
+  import Header from '~/components/Header'
+
+  export default {
+    components: {
+      Header,
+    }
+  }
+</script>
 
 <style>
 html {
