41e0d99b2560b98a7587573f41f2d9c4371f5d24
lechinoix <nicolas.ngomai@gmail.com>
2019-01-21 22:33:00 +0100

    Add comments

---
+++ components/Comment.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ comment.body }}
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    comment: Object
+  },
+  mounted: function () {console.log('this.comment', this.comment)}
+}
+</script>
--- pages/comments.vue
+++ pages/comments.vue
@@ -1,21 +1,39 @@
 <template>
   <section class="container">
+    <h1>{{ post.title }}</h1>
+    <div
+      class="comment"
+      v-for="comment in comments"
+      :key="comment.id"
+    >
+      <Comment :comment="comment" />
+    </div>
   </section>
 </template>
 
 <script>
 import { getComments } from '~/services/redditClient.js'
+import Comment from '~/components/comment'
 
 export default {
   components: {
+    Comment
   },
+  data: () => ({
+    comments: [],
+    post: {}
+  }),
   mounted: async function() {
-    let comments = await getComments('pwa', 'a408bd/you_need_neither_pwa_nor_amp_to_make_your_website')
-    console.log(comments)
+    const [[post], comments] = await getComments('pwa', 'a408bd/you_need_neither_pwa_nor_amp_to_make_your_website')
+    this.post = post
+    this.comments = comments
   }
 }
 </script>
 
 <style>
+.comment {
+  margin: 25px;
+}
 </style>
 
